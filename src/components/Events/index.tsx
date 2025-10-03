c7c596ab7b6752a947d06a836406df74b6e66586
Thiago Queiroz <thiagojqueiroz@outlook.com>
2025-10-02 22:51:52 -0300

    Update: Atletacard v2

--- src/components/Events/index.tsx
+++ src/components/Events/index.tsx
@@ -19,7 +19,7 @@ export default function Events() {
         <p className={`text-6xl 2xl:text-8xl font-bold text-zinc-100`}>Eventos</p>
       </span>
 
-      <div className={`w-full p-2 grid grid-cols-2 lg:grid-cols-5 justify-center items-center gap-1`}>
+      <div className={`w-full p-2 grid grid-cols-1 lg:grid-cols-5 justify-center items-center gap-1`}>
         <span className={`w-full flex-col text-center justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none px-2 py-4 bg-sky-500/80`}>
           <Image src={`/evento001.svg`} width={480} height={480} alt="" className={`mx-auto hover:cursor-pointer`} priority />
           <div className={`flex flex-col justify-center items-center`}>
