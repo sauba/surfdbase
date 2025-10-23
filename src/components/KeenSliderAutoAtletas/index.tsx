7d3f4bbdc3cd1bf1e335147fd3b8fd798de0d121
Thiago Queiroz <thiagojqueiroz@outlook.com>
2025-10-22 23:40:14 -0300

    Update: KeenSliderAutoAtletas information about members v3

--- src/components/KeenSliderAutoAtletas/index.tsx
+++ src/components/KeenSliderAutoAtletas/index.tsx
@@ -30,7 +30,7 @@ export default function KeenSliderAutoAtletas() {
   return (
     <div ref={sliderRef} className="keen-slider">
       <div className="keen-slider__slide number-slide1">
-        <span className={`w-full h-96 flex text-center justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
+        <span className={`w-full h-96 flex text-left justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
           <Image src={`/messias.svg`} width={180} height={180} alt="" className={`mx-auto hover:cursor-pointer`} priority />
           <div className={`flex flex-col justify-center items-center`}>
             <span className={`text-lg xl:text-xl 2xl:text-2xl font-pt-mono text-zinc-100`}>Messias</span>
@@ -43,7 +43,7 @@ export default function KeenSliderAutoAtletas() {
         </span>
       </div>
       <div className="keen-slider__slide number-slide2">
-        <span className={`w-full h-96 flex text-center justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
+        <span className={`w-full h-96 flex text-left justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
           <Image src={`/paulinho.svg`} width={180} height={180} alt="" className={`mx-auto hover:cursor-pointer`} priority />
           <div className={`flex flex-col justify-center items-center`}>
             <span className={`text-lg xl:text-xl 2xl:text-2xl font-pt-mono text-zinc-100`}>Luis Paulo Dias</span>
@@ -56,7 +56,7 @@ export default function KeenSliderAutoAtletas() {
         </span>
       </div>
       <div className="keen-slider__slide number-slide3">
-        <span className={`w-full h-96 flex text-center justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
+        <span className={`w-full h-96 flex text-left justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
           <Image src={`/yuri.svg`} width={180} height={180} alt="" className={`mx-auto hover:cursor-pointer`} priority />
           <div className={`flex flex-col justify-center items-center`}>
             <span className={`text-lg xl:text-xl 2xl:text-2xl font-pt-mono text-zinc-100`}>Yuri</span>
@@ -69,7 +69,7 @@ export default function KeenSliderAutoAtletas() {
         </span>
       </div>
       <div className="keen-slider__slide number-slide4">
-        <span className={`w-full h-96 flex text-center justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
+        <span className={`w-full h-96 flex text-left justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
           <Image src={`/nicolas.svg`} width={180} height={180} alt="" className={`mx-auto hover:cursor-pointer`} priority />
           <div className={`flex flex-col justify-center items-center`}>
             <span className={`text-lg xl:text-xl 2xl:text-2xl font-pt-mono text-zinc-100`}>Nicolas</span>
@@ -82,7 +82,7 @@ export default function KeenSliderAutoAtletas() {
         </span>
       </div>
       <div className="keen-slider__slide number-slide5">
-        <span className={`w-full h-96 flex text-center justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
+        <span className={`w-full h-96 flex text-left justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
           <Image src={`/jiskjr.svg`} width={180} height={180} alt="" className={`mx-auto hover:cursor-pointer`} priority />
           <div className={`flex flex-col justify-center items-center`}>
             <span className={`text-lg xl:text-xl 2xl:text-2xl font-pt-mono text-zinc-100`}>Gabriel Medeiros</span>
@@ -95,7 +95,7 @@ export default function KeenSliderAutoAtletas() {
         </span>
       </div>
       <div className="keen-slider__slide number-slide6">
-        <span className={`w-full h-96 flex text-center justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
+        <span className={`w-full h-96 flex text-left justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
           <Image src={`/ryan.svg`} width={180} height={180} alt="" className={`mx-auto hover:cursor-pointer`} priority />
           <div className={`flex flex-col justify-center items-center`}>
             <span className={`text-lg xl:text-xl 2xl:text-2xl font-pt-mono text-zinc-100`}>Nome: Ryan</span>
@@ -108,7 +108,7 @@ export default function KeenSliderAutoAtletas() {
         </span>
       </div>
       <div className="keen-slider__slide number-slide7">
-        <span className={`w-full h-96 flex text-center justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
+        <span className={`w-full h-96 flex text-left justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
           <Image src={`/joao.svg`} width={180} height={180} alt="" className={`mx-auto hover:cursor-pointer`} priority />
           <div className={`flex flex-col justify-center items-center`}>
             <span className={`text-lg xl:text-xl 2xl:text-2xl font-pt-mono text-zinc-100`}>João</span>
@@ -121,10 +121,10 @@ export default function KeenSliderAutoAtletas() {
         </span>
       </div>
       <div className="keen-slider__slide number-slide8">
-        <span className={`w-full h-96 flex text-center justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
+        <span className={`w-full h-96 flex text-left justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
           <Image src={`/heitor.svg`} width={180} height={180} alt="" className={`mx-auto hover:cursor-pointer`} priority />
           <div className={`flex flex-col justify-center items-center`}>
-            <span className={`text-lg xl:text-xl 2xl:text-2xl font-pt-mono text-zinc-100`}>Nome: Heitor Gomes Dantas Lima</span>
+            <span className={`text-lg xl:text-xl 2xl:text-2xl font-pt-mono text-zinc-100`}>Heitor Gomes Dantas Lima</span>
             <span className={`text-lg xl:text-xl 2xl:text-2xl font-pt-mono text-zinc-100`}>10 Anos</span>
             <span className={`text-lg xl:text-xl 2xl:text-2xl font-pt-mono text-zinc-100`}>Base: Regular</span>
             <span className={`text-lg xl:text-xl 2xl:text-2xl font-pt-mono text-zinc-100`}>Apoio|Patrocínio: Shark SurfBoard, Diretoria Surf Point, Gromettsurf e Oxe Surfboard</span>
@@ -134,7 +134,7 @@ export default function KeenSliderAutoAtletas() {
         </span>
       </div>
       <div className="keen-slider__slide number-slide9">
-        <span className={`w-full h-96 flex text-center justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
+        <span className={`w-full h-96 flex text-left justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
           <Image src={`/miguel.svg`} width={180} height={180} alt="" className={`mx-auto hover:cursor-pointer`} priority />
           <div className={`flex flex-col justify-center items-center`}>
             <span className={`text-lg xl:text-xl 2xl:text-2xl font-pt-mono text-zinc-100`}>Miguel Gomes Dantas Lima</span>
@@ -147,7 +147,7 @@ export default function KeenSliderAutoAtletas() {
         </span>
       </div>
       {/* <div className="keen-slider__slide number-slide10">
-        <span className={`w-full h-96 flex text-center justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
+        <span className={`w-full h-96 flex text-left justify-between xl:justify-center items-center gap-3 rounded-lg border-none outline-none bg-sky-500/80`}>
           <Image src={`/surfer.svg`} width={180} height={180} alt="" className={`mx-auto hover:cursor-pointer`} priority />
           <div className={`flex flex-col justify-center items-center`}>
             <span className={`text-lg xl:text-xl 2xl:text-2xl font-pt-mono text-zinc-100`}>Nome: 10</span>
